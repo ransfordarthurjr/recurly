53096e5fe399800cb1d91bf2c3839bfeb61745de
Ransford Arthur Jr <ransford.arthur.jr@gmail.com>
2026-04-11 20:45:21 +0000

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,49 +1,32 @@
 import '@/global.css';
 import { Link } from 'expo-router';
+import { styled } from 'nativewind';
 import { Text } from 'react-native';
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
-import { styled } from 'nativewind';
 
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
     return (
         <SafeAreaView className="flex-1 bg-background p-5">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
-            </Text>
+            <Text className="text-5xl font-sans-bold">Home</Text>
 
             <Link
                 href="/onboarding"
-                className="mt-4 rounded bg-primary text-white p-4">
+                className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
                 Go to Onboarding
             </Link>
 
             <Link
                 href="/(auth)/sign-in"
-                className="mt-4 rounded bg-primary text-white p-4">
+                className="mt-4   font-sans-bold rounded bg-primary text-white p-4">
                 Go to Sign In
             </Link>
 
             <Link
                 href="/(auth)/sign-up"
-                className="mt-4 rounded bg-primary text-white p-4">
+                className="mt-4  font-sans-bold rounded bg-primary text-white p-4">
                 Go to Sign Up
-            </Link>
-
-            <Link
-                href="/subscriptions/spotify"
-                className="mt-4 rounded bg-primary text-white p-4">
-                Spotify Subscription
-            </Link>
-
-            <Link
-                href={{
-                    pathname: '/subscriptions/[id]',
-                    params: { id: 'claude' },
-                }}
-                className="mt-4 rounded bg-primary text-white p-4">
-                Cluade Max Subscription
             </Link>
         </SafeAreaView>
     );
